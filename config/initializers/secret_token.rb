a56de0d89e20ebed8d474390550a758c7c9cf4c9
Nathan Fritz <nathan@thefritzes.net>
2012-04-25 12:38:23 -0500

    Initial Rails Generation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExposednessOnBalustrades::Application.config.secret_token = '63f81856d5479792d02116c05544ae44084cc214918e289564cc56ca90633ca1e21f43929e50bfc949b82032220f9074cfb7aa07a1ee80765a4df765e3cbf033'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ExposednessOnBalustrades::Application.config.session_store :cookie_store, key: '_exposedness-on-balustrades_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ExposednessOnBalustrades::Application.config.session_store :active_record_store
